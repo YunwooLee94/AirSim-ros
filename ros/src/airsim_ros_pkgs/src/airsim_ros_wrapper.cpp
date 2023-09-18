a8697a57cf87c8ccf9a2a76828484dc71d9d2024
larr-laptop <yunwoo333@gmail.com>
2023-09-18 15:43:47 +0900

    Add keyboard operator

--- ros/src/airsim_ros_pkgs/src/airsim_ros_wrapper.cpp
+++ ros/src/airsim_ros_pkgs/src/airsim_ros_wrapper.cpp
@@ -112,7 +112,6 @@ void AirsimROSWrapper::initialize_ros()
     std::cout << std::endl;
     object_name_set = object_name_set_temp;
 
-
     create_ros_pubs_from_settings_json();
     airsim_control_update_timer_ = nh_private_.createTimer(ros::Duration(update_airsim_control_every_n_sec), &AirsimROSWrapper::drone_state_timer_cb, this);
 }
@@ -121,9 +120,9 @@ void AirsimROSWrapper::initialize_ros()
 void AirsimROSWrapper::create_ros_pubs_from_settings_json()
 {
     // subscribe to control commands on global nodehandle
-    gimbal_angle_quat_cmd_sub_ = nh_private_.subscribe("gimbal_angle_quat_cmd", 50, &AirsimROSWrapper::gimbal_angle_quat_cmd_cb, this);
-    gimbal_angle_euler_cmd_sub_ = nh_private_.subscribe("gimbal_angle_euler_cmd", 50, &AirsimROSWrapper::gimbal_angle_euler_cmd_cb, this);
-    origin_geo_point_pub_ = nh_private_.advertise<airsim_ros_pkgs::GPSYaw>("origin_geo_point", 10);
+    gimbal_angle_quat_cmd_sub_ = nh_private_.subscribe("gimbal_angle_quat_cmd", 1, &AirsimROSWrapper::gimbal_angle_quat_cmd_cb, this);
+    gimbal_angle_euler_cmd_sub_ = nh_private_.subscribe("gimbal_angle_euler_cmd", 1, &AirsimROSWrapper::gimbal_angle_euler_cmd_cb, this);
+    origin_geo_point_pub_ = nh_private_.advertise<airsim_ros_pkgs::GPSYaw>("origin_geo_point", 1);
 
     for (auto str: object_name_set){
         std::string topic = str + "_pose";
